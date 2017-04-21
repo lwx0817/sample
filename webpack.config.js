fa16b098622364f94cff2cf83d03cf320af369b3
lwx0817 <liuwenxu@skr365.cn>
2017-04-21 14:37:08 +0800

    test

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.	 = {
+  devtool: 'eval-source-map',
+
+  entry:  __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+
+  module: {//在配置文件里添加JSON loader
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',//在webpack的module部分的loaders里进行配置即可
+        query: {
+          presets: ['es2015','react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  }
+}
